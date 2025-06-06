07bd9e25449a097f743637d7662e2151801da783
niccoloanceschi <58816223+niccoloanceschi@users.noreply.github.com>
2025-06-06 16:57:14 -0400

    Edited tutorial
    - Added Grant White School application
    - Added jointRot_NOnorm function

--- helper_fcts.R
+++ helper_fcts.R
@@ -66,9 +66,26 @@ to_infinitefact <- function(lambdaMat, k, p){
   
 }
 
+jointRot_NOnorm = function(lambda, eta){
+  vari = lapply(lambda, varimax, normalize = F)
+  loads = lapply(vari, `[[`, 1)
+  rots = lapply(vari, `[[`, 2)
+  rotfact = mapply(`%*%`, eta, rots, SIMPLIFY = FALSE)
+  
+  norms = sapply(loads, norm, "2")
+  piv = loads[order(norms)][[round(length(lambda)/2)]]
+  
+  matches = lapply(loads, msfOUT, piv)
+  
+  lamout = mapply(aplr, loads, matches, SIMPLIFY = FALSE)
+  etaout = mapply(aplr, rotfact, matches, SIMPLIFY = FALSE)
+  
+  return(list(lambda = lamout, eta = etaout))
+}
+
 jointRot_pivot = function (lambda, eta, piv = NULL) 
 {
-  vari = lapply(lambda, varimax)
+  vari = lapply(lambda, varimax, normalize = F)
   loads = lapply(vari, `[[`, 1)
   rots = lapply(vari, `[[`, 2)
   rotfact = mapply(`%*%`, eta, rots, SIMPLIFY = FALSE)
